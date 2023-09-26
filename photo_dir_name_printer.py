ddf0728f75e43f3bd5a7051cc0db5d76cec9717b
Justin Warner <justin.warner@getcruise.com>
2023-09-26 12:47:59 -0700

    working on new deleted checker

--- photo_dir_name_printer.py
+++ photo_dir_name_printer.py
@@ -1,18 +1,21 @@
 import os, pathlib
 
+# prints all files and directories (including subfiles and directories) in the given path indented by depth
+
 # SOURCE_PATH = 'D:/Pictures/All/Special/'
 SOURCE_PATH = 'D:/Pictures/'
+SOURCE_PATH = '/Users/justin.warner/Pictures/2023_09_15 Vancouver Island Port Hardy Diving/'
 
 def printContents(directory):
     dirCount = 0
     fileCount = 0
-    for root, dirs, files in os.walk(directory):
+    for root, dirs, files in sorted(os.walk(directory)):
         level = root.replace(directory, '').count(os.sep)
         indent = "    " * level
         print(f"{indent}{pathlib.PureWindowsPath(root)}")
         dirCount += 1
         fileCount += len(files)
-        for file in files:
+        for file in sorted(files):
             print(f"{indent}    {file}")
     print('{} directories (including root) and {} files'.format(dirCount, fileCount))
 
--- photo_filename_printer.py
+++ photo_filename_printer.py
@@ -1,5 +1,7 @@
 from os import walk
 
+# prints all files at root level in the given path
+
 SOURCE_PATH = 'D:/Pictures/All/Underwater/ID Guide Puget Sound/'
 
 _, _, filenames = next(walk(SOURCE_PATH))
